5a3bd3a5ee95123d6dc1529677a18d3bf8abc108
Atuh Samuel <atuhsamuel96@gmail.com>
2023-01-10 22:53:37 +0100

    Made modifications

--- bincomTest/urls.py
+++ bincomTest/urls.py
@@ -22,12 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('list_polls/', views.list_pools, name='list_polls'),
     path('summed_results/<int:lga_id>/', views.summed_results, name='summed_results'),
-    # path('add_items/', views.add_items, name='add_items'),
-    # path('update_items/<str:pk>/', views.update_items, name="update_items"),
-    # path('delete_items/<str:pk>/', views.delete_items, name="delete_items"),
     path('polling_unit_detail/<str:polling_unit_unique_id>/', views.polling_unit_detail, name="polling_unit_detail"),
-    # path('issue_items/<str:pk>/', views.issue_items, name="issue_items"),
-    # path('receive_items/<str:pk>/', views.receive_items, name="receive_items"),
-    # path('reorder_level/<str:pk>/', views.reorder_level, name="reorder_level"),
-    # path('accounts/', include('registration.backends.default.urls')),
 ]
